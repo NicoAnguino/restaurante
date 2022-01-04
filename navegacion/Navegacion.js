3cd959af3942a586c26cd40a1d2b6d3f4a028725
NicoAnguino <nikolas_28_95@hotmail.com>
2022-01-04 14:49:59 -0300

    Iconos

--- navegacion/Navegacion.js
+++ navegacion/Navegacion.js
@@ -8,13 +8,54 @@ import Favoritos from '../screens/Favoritos'
 import TopRestaurantes from '../screens/TopRestaurantes'
 import Buscar from '../screens/Buscar'
 import Account from '../screens/Account'
+import { Icon } from 'react-native-elements/dist/icons/Icon'
 
 const Tab = createBottomTabNavigator()
 
 export default function Navegacion() {
+
+    const screenOptions = (route, color) => {
+        let nombreIcono 
+        switch (route.name) {
+            case "restaurantes":
+                nombreIcono = "compass-outline"
+                break;
+            case "favoritos":
+                nombreIcono = "heart-outline"
+                break;
+            case "top-restaurantes":
+                nombreIcono = "star-outline"
+                break;
+            case "buscar":
+                nombreIcono = "magnify"
+                break;
+            case "account":
+                nombreIcono = "home-outline"
+                break;
+        }
+
+        return (
+            <Icon 
+                type='material-community'
+                name={nombreIcono}
+                size={22}
+                color={color}
+            />
+        )
+    }
+
     return (       
        <NavigationContainer>
-           <Tab.Navigator>
+           <Tab.Navigator
+                initialRouteName='restaurantes'
+                tabBarOptions={{
+                    inactiveTintColor: "#e8bca4",
+                    activeTintColor: "#d11f0d"
+                }}
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({color}) => screenOptions(route, color)
+                })}
+           >
                 <Tab.Screen 
                     name="restaurantes"
                     component={Restaurantes}
