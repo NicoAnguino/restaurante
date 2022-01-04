ff2f5280151c364a74e81e00d7fd6fb0c582a0e3
NicoAnguino <nikolas_28_95@hotmail.com>
2022-01-04 16:50:42 -0300

    Vista Cuenta

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,32 +1,22 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-//import { esUsuarioLogueado } from '../../utilidades/acciones'
-
-import firebase from 'firebase/app'
-
 import Loading from '../../componentes/Loading'
+import { buscarUsuarioActual } from '../../utilidades/acciones'
 
 import UsuarioInvitado from './UsuarioInvitado'
 import UsuarioLogueado from './UsuarioLogueado'
-import { buscarUsuarioActual } from '../../utilidades/acciones'
 
 export default function Account() {
     const [logueo, setLogueo] = useState(null)
     
-    useEffect(() => {
-        
+    useEffect(() => {  
         const usuarioActual = buscarUsuarioActual()
         usuarioActual ? setLogueo(true) : setLogueo(false)
     }, [])
 
-    //firebase.auth().onAuthStateChanged((user) => {
-      //  user !== null ? (setLogueo(true)) : setLogueo(false)
-    //})
-
-if(logueo == null){
-    //return <Text>Cargando...</Text>
-    return <Loading isVisible={true} text="Cargando..." />
-}
+    if(logueo == null){
+        return <Loading isVisible={true} text="Cargando..." />
+    }
 
     return logueo ? <UsuarioLogueado /> : <UsuarioInvitado />
 }
--- screens/account/UsuarioInvitado.js
+++ screens/account/UsuarioInvitado.js
@@ -1,12 +1,54 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView,Image } from 'react-native'
+//import { Button } from 'react-native-elements/dist/buttons/Button'
+import { Button } from 'react-native-elements'
 
 export default function UsuarioInvitado() {
     return (
-        <View>
-            <Text>Usuario Invitado!</Text>           
-        </View>
+        <ScrollView
+            centerContent
+            style={styles.viewBody}
+        >
+            <Image 
+                source={require("../../assets/restaurant-logo.png")}
+                resizeMode='contain'
+                style={styles.image}
+            />
+            <Text style={styles.title}>Consulta tu perfil en Restaurantes</Text>
+            <Text style={styles.descripcion}>¿Cómo describirías tu mejor Restaurante? Busca y visualiza los mejores restaurantes de una forma sencilla,
+            votá cuál te ha gustado más y comenta cómo ha sido tu experiencia</Text>
+            <Button 
+                title="Ver tu perfil"
+                buttonStyle={styles.bottonInicio}
+                onPress={()=>console.log("Click!!!")}
+            />
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody:{
+        marginHorizontal:30
+    },
+    image:{
+        height:300,
+        width:"100%",
+        marginBottom:10,
+        textAlign:"center"
+    },
+    title:{
+        fontWeight:"bold",
+        fontSize:19,
+        marginVertical:10,
+        textAlign:"center"
+    },
+    descripcion:{
+        textAlign:"justify",
+        marginVertical:10,
+        marginBottom:20,
+        color: "#d11f0d"
+    },
+    bottonInicio:{
+        backgroundColor:"#d11f0d"
+    }
+})
