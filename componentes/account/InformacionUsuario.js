6db8a6c69001596c21b1bd69bd1dbf40ecf84219
Nico Anguino <nikolas_28_95@hotmail.com>
2022-01-07 16:24:41 -0300

    Trabajando en la vista de usuario logueado

---
+++ componentes/account/InformacionUsuario.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function InformacionUsuario({usuario}) {
+    return (
+        <View>
+            <Text>Información del usuario...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
--- screens/account/UsuarioLogueado.js
+++ screens/account/UsuarioLogueado.js
@@ -1,23 +1,71 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
-import { cerrarSesion } from '../../utilidades/acciones'
+import Toast from 'react-native-easy-toast'
+
+import { buscarUsuarioActual, cerrarSesion } from '../../utilidades/acciones'
+import Loading from '../../componentes/Loading'
+import InformacionUsuario from '../../componentes/account/InformacionUsuario'
 
 export default function UsuarioLogueado() {
+    const toastRef = useRef()
     const navegacion = useNavigation()
+
+    const [cargando, setCargando] = useState(false)
+    const [cargandoTexto, setCargandoTexto] = useState("")
+    const [usuario, setUsuario] = useState(null)
+
+    useEffect(() => {
+        setUsuario(buscarUsuarioActual())
+    }, [])
+
     return (
-        <View>
-            <Text>Hola Usuario Logueado!</Text>
+        <View style={styles.container}>
+
+            {
+               usuario && <InformacionUsuario usuario={usuario} />
+            }
+
+            <Text>Opciones de Cuenta</Text>
             <Button 
                 title="Cerrar Sesión"
+                buttonStyle={styles.btnCerrarSesion}
+                titleStyle={styles.btnCerrarSesionTitulo}
                 onPress={() => {
                     cerrarSesion()
                     navegacion.navigate("restaurantes")
                 }}
             />
+            <Toast
+                ref={toastRef}
+                position='center'
+                opacity={0.9}
+            />
+            <Loading 
+                isVisible={cargando}
+                text={cargandoTexto}
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        minHeight:"100%",
+        backgroundColor:"#f9f9f9"
+    },
+    btnCerrarSesion:{
+        marginTop:30,
+        borderRadius:5,
+        backgroundColor:"#ffffff",
+        borderTopWidth:1,
+        borderTopColor:"#d64a34",
+        borderBottomWidth:1,
+        borderBottomColor:"#d64a34",
+        paddingVertical:10
+    },
+    btnCerrarSesionTitulo:{
+        color:"#d64a34",
+    }
+})
