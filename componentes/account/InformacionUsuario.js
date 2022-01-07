1560e86e70a9c3316fc81f4df995411f8b043a1a
Nico Anguino <nikolas_28_95@hotmail.com>
2022-01-07 16:44:39 -0300

    Mostrar Info Usuario

--- componentes/account/InformacionUsuario.js
+++ componentes/account/InformacionUsuario.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
 
 export default function InformacionUsuario({usuario}) {
+       
     return (
-        <View>
-            <Text>Información del usuario...</Text>
+        <View style={styles.container}>
+            <Avatar
+                rounded={true}
+                size="large"
+                source={
+                    usuario.photoURL
+                    ? {url: usuario.photoURL}
+                    : require("../../assets/avatar-default.jpg")
+                }
+            />
+            <View style={styles.infoUsuario}>
+                <Text style={styles.displayName}>{ usuario.displayName ? usuario.displayName : "Anónimo" }</Text>
+                <Text>{ usuario.email }</Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"row",
+        backgroundColor:"#f9f9f9",
+        paddingVertical:30
+    },
+    infoUsuario:{
+        marginLeft:20
+    },
+    displayName:{
+        fontWeight:"bold",
+        paddingBottom:5
+    }
+})
