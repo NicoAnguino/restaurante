53ff8a6c8394865d268ecfd8f779241d3a170b8d
Nico Anguino <nikolas_28_95@hotmail.com>
2022-01-07 17:55:31 -0300

    Libreria y Método para subir imagen desde teléfono

--- componentes/account/InformacionUsuario.js
+++ componentes/account/InformacionUsuario.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { importarImagenGaleria } from '../../utilidades/helpers'
 
 export default function InformacionUsuario({usuario}) {
        
+    const cambiarFoto = async() => {
+        const result = await importarImagenGaleria([1,1])
+        console.log(result)
+    }
+
     return (
         <View style={styles.container}>
             <Avatar
                 rounded={true}
                 size="large"
+                onPress={() => cambiarFoto()}
                 source={
                     usuario.photoURL
                     ? {url: usuario.photoURL}
