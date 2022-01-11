f16d55045d93b653f81d773a22fd9927619bf30a
Nico Anguino <nikolas_28_95@hotmail.com>
2022-01-11 17:02:32 -0300

    Capturar datos y objeto formulario restaurante

--- componentes/restaurantes/NuevoRestauranteForm.js
+++ componentes/restaurantes/NuevoRestauranteForm.js
@@ -4,14 +4,28 @@ import { Input,Button } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
 
 export default function NuevoRestauranteForm({toastRef, setCargando, navigation}) {
-    
+    const [datosFormulario, setDatosFormulario] = useState(valoresDefectoFormulario())
+    const [errorNombre, setErrorNombre] = useState(null)
+    const [errorDescripcion, setErrorDescripcion] = useState(null)
+    const [errorEmail, setErrorEmail] = useState(null)
+    const [errorTelefono, setErrorTelefono] = useState(null)
+    const [errorDireccion, setErrorDireccion] = useState(null)
+
     const agregarRestaurante = () => {
-        console.log("Guuardar Restaurante!")
+        console.log(datosFormulario)
     }
     
     return (
         <View style={styles.vistaContenedora}>
-            <AgregarFormulario/>
+            <AgregarFormulario
+                datosFormulario={datosFormulario}
+                setDatosFormulario={setDatosFormulario}
+                errorNombre={errorNombre}
+                errorDescripcion={errorDescripcion}
+                errorEmail={errorEmail}
+                errorTelefono={errorTelefono}
+                errorDireccion={errorDireccion}
+            />
             <Button 
                 title="Crear Restaurante"
                 onPress={agregarRestaurante}
@@ -21,22 +35,35 @@ export default function NuevoRestauranteForm({toastRef, setCargando, navigation}
     )
 }
 
-function AgregarFormulario() {
+function AgregarFormulario({datosFormulario,setDatosFormulario,errorNombre,errorDescripcion,errorEmail,errorTelefono,errorDireccion}) {
     const [pais, setPais] = useState("AR")
     const [codigoLlamada, setCodigoLlamada] = useState("54")
     const [telefono, setTelefono] = useState("")
+
+    const onChange = (e, type) =>{
+        setDatosFormulario({...datosFormulario, [type]: e.nativeEvent.text})
+    }
 
     return (
         <View style={styles.formulario}>
             <Input
                 placeholder="Nombre del Restaurante"
+                defaultValue={datosFormulario.nombre}
+                onChange={(e) => onChange(e, "nombre")}
+                errorMessage={errorNombre}
             />
             <Input
                 placeholder="Dirección del Restaurante"
+                defaultValue={datosFormulario.direccion}
+                onChange={(e) => onChange(e, "direccion")}
+                errorMessage={errorDireccion}
             />
             <Input
                 keyboardType="email-address"
                 placeholder="Email del Restaurante"
+                defaultValue={datosFormulario.email}
+                onChange={(e) => onChange(e, "email")}
+                errorMessage={errorEmail}
             />
             <View style={styles.vistaTelefono}>
                 <CountryPicker 
@@ -47,6 +74,7 @@ function AgregarFormulario() {
                   containerStyle={styles.countryPicker}
                   countryCode={pais}
                   onSelect={(country) => {
+                        setDatosFormulario({...datosFormulario, "pais":country.cca2, "codigoInternacional":country.callingCode[0]})
                         setPais(country.cca2)
                         setCodigoLlamada(country.callingCode[0])
                     }}
@@ -55,15 +83,33 @@ function AgregarFormulario() {
                     placeholder="WhatsApp del restaurante..."
                     keyboardType="phone-pad"
                     containerStyle={styles.inputTelefono}
+                    defaultValue={datosFormulario.telefono}
+                    onChange={(e) => onChange(e, "telefono")}
+                    errorMessage={errorTelefono}
                 /> 
             </View>
             <Input
                     placeholder="Descripción restaurante..."
                     multiline
                     containerStyle={styles.textArea}
+                    defaultValue={datosFormulario.descripcion}
+                    onChange={(e) => onChange(e, "descripcion")}
+                    errorMessage={errorDescripcion}
                 />
         </View>
     )
+}
+
+const valoresDefectoFormulario = () => {
+    return {
+        nombre:"",
+        descripcion:"",
+        telefono:"",
+        direccion:"",
+        pais:"AR",
+        codigoInternacional:"54",
+        email:""
+    }
 }
 
 const styles = StyleSheet.create({
